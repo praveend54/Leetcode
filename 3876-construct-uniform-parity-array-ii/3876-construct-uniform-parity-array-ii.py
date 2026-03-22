2c8db1a6f95e67c12a360039ffdf7ab8e6fc6b00
Praveen Dharmavarapu <126992840+praveend54@users.noreply.github.com>
2026-03-22 19:08:03 +0530

    Time: 16 ms (88.25%), Space: 35.5 MB (37.58%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,13 +1,9 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        m=float('inf')
-        e=0
+        s=min(nums1)
+        if s%2==1:
+            return True
         for i in nums1:
-            m=min(m,i)
-            if i%2==0:
-                e+=1
-        if m%2!=0:
-            return True
-        if e==len(nums1):
-            return True
-        return False
+            if i%2==1:
+                return False
+        return True
