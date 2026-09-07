ad825a540892a5e15c146f95173025cc041f44b7
Praveen Dharmavarapu <126992840+praveend54@users.noreply.github.com>
2026-09-07 16:11:53 +0530

    Time: 233 ms (83.87%), Space: 21.6 MB (73.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
         n=len(grid)
         m=len(grid[0])
-        c=0
-        def dfs(grid,i,j):
-            if i<0 or j<0 or i>n-1 or j>m-1 or grid[i][j]=="0":
-                return 
+        def dfs(i,j):
+            if i>=n or j>=m or i<0 or j<0 or grid[i][j]=="0":
+                return
             grid[i][j]="0"
-            dfs(grid,i+1,j)
-            dfs(grid,i,j+1)
-            dfs(grid,i-1,j)
-            dfs(grid,i,j-1)
+            dfs(i-1,j)
+            dfs(i+1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
+        c=0
         for i in range(n):
             for j in range(m):
                 if grid[i][j]=="1":
+                    dfs(i,j)
                     c+=1
-                    dfs(grid,i,j)
-        def dfs(grid,i,j):
-            if i<0 or j<0 or i>n-1 or j>m-1 or grid[i][j]=="0":
-                return 
-            grid[i][j]="0"
-            dfs(grid,i+1,j)
-            dfs(grid,i,j+1)
-            dfs(grid,i-1,j)
-            dfs(grid,i,j-1)
         return c
